355a540d274dc1d3cb65b992a249384e6a473c25
Daniel2711 <detaddeo.d@gmail.com>
2021-02-24 14:12:23 +0100

    Inserita sezione

--- dist/app.js
+++ dist/app.js
@@ -16,7 +16,85 @@ new vue__WEBPACK_IMPORTED_MODULE_0__.default({
   el: '#app',
   data: {
     message: 'testo di prova',
-    navItem: ['produts', 'brushes', 'wacom tablets', 'special offerts', 'free trial', 'business', 'support', 'learning']
+    navItem: ['produts', 'brushes', 'wacom tablets', 'special offerts', 'free trial', 'business', 'support', 'learning'],
+    cards1: [{
+      title: 'Painter essentials 8',
+      descrition: 'The best affordable painting software for beginners',
+      label: true
+    }, {
+      title: 'Particle shop',
+      descrition: 'Powerful adobe photoshop brush plugin powered by Painter',
+      label: false
+    }, {
+      title: 'Brush packs',
+      descrition: 'Diverse add-on packs to embellish photo composites, grafic designer and painting',
+      label: false
+    }],
+    footer: [{
+      title: 'Products',
+      items: ['Corel Painter', 'ParticleShop', 'Painter Essentials', 'Special Offers', 'Free Trials', '                    All products']
+    }, {
+      title: 'Quick Links',
+      items: ['Licensing', 'Tutorials & Tips', 'Webinars', 'The Painter Factory', 'Discovery Center', 'Beta Programs']
+    }, {
+      title: 'Support',
+      items: ['Register your product', 'Patches & Updates', 'Knowledgebase', 'Uninstall instructions', 'Contact us']
+    }, {
+      title: 'About Corel',
+      items: ['Company information', 'Newsroom', 'Careers', 'Partner Program', 'Legal Information', 'EULA']
+    }],
+    socials: [{
+      icon: "<i class=\"fab fa-instagram\"></i>",
+      name: 'logo istragram',
+      color: 'social-icon bg-pink'
+    }, {
+      icon: "<i class=\"fab fa-facebook-f\"></i>",
+      name: 'logo facebook',
+      color: 'social-icon bg-blue'
+    }, {
+      icon: "<i class=\"fab fa-twitter\"></i>",
+      name: 'logo twitter',
+      color: 'social-icon bg-light-blue'
+    }, {
+      icon: "<i class=\"fab fa-youtube\"></i>",
+      name: 'logo youtube',
+      color: 'social-icon bg-red'
+    }, {
+      icon: "<i class=\"fab fa-pinterest-p\"></i>",
+      name: 'logo pinterest',
+      color: 'social-icon bg-red-dark'
+    }, {
+      icon: "<i class=\"fas fa-rss\"></i>",
+      name: 'logo painter blog',
+      color: 'social-icon bg-orange'
+    }] // Products
+    //     Corel Painter
+    //     ParticleShop
+    //     Painter Essentials
+    //     Special Offers
+    //     Free Trials
+    //     All products
+    //Quick Links
+    //     Licensing
+    //     Tutorials & Tips
+    //     Webinars
+    //     The Painter Factory
+    //     Discovery Center
+    //     Beta Programs
+    //Support
+    //     Register your product
+    //     Patches & Updates
+    //     Knowledgebase
+    //     Uninstall instructions
+    //     Contact us
+    //About Corel
+    //     Company information
+    //     Newsroom
+    //     Careers
+    //     Partner Program
+    //     Legal Information
+    //     EULA
+
   }
 });
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,10 @@ let mix = require('laravel-mix');
 // mix.js('src/app.js', 'dist');
 mix.js('src/app.js', 'dist').vue();
 
-mix.sass('src/scss/style.scss', 'dist');
+mix
+   .sass('src/scss/jumbotron.scss', 'dist')
+   .sass('src/scss/card.scss','dist')
+   .sass('src/scss/style.scss', 'dist')
+   .options({
+        processCssUrls: false
+   });
