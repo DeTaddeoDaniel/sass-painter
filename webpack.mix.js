cd517723c815f88d93d0bab6f317617af57a49d4
Daniel2711 <detaddeo.d@gmail.com>
2021-02-24 19:53:11 +0100

    Fix error sitemato module

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,9 +4,4 @@ let mix = require('laravel-mix');
 mix.js('src/app.js', 'dist').vue();
 
 mix
-   .sass('src/scss/jumbotron.scss', 'dist')
-   .sass('src/scss/card.scss','dist')
    .sass('src/scss/style.scss', 'dist')
-   .options({
-        processCssUrls: false
-   });
